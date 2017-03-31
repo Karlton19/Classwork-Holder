63c5eeba29ac4f78ea718747a33402b4ace35674
Karlton19 <karltond15@gmail.com>
2017-03-31 15:07:11 -0400

    Add files via upload

--- Code_Search_Drewniak.cpp
+++ Code_Search_Drewniak.cpp
@@ -12,15 +12,26 @@ int i;
 }
 
 void search::linear(int num){
-
 	int i;
+	int index = 0;
+	int calc_num = 0;
+
 
 	for (i = 0; i < 20; i++) {
-		if (array[i] == num){
-			cout << "Number is %d" << num;
-		}
+		if (array[i] == num);
+			calc_num = calc_num + 1;
+		if (index >= array[20] || index !=num);
+			
+
+	cout << "Number is: " << num << "\n";
+	cout << "Number of Calculations test: " << calc_num << "\n";
+
 	}
+
 }
+
+
+
 
 int main() {
 int num = 6;
