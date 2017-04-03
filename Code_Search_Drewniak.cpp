6a5e49dfa8c57a7627bf314758dbea0851cba405
Karlton19 <karltond15@gmail.com>
2017-04-03 15:23:06 -0400

    Add files via upload

--- Code_Search_Drewniak.cpp
+++ Code_Search_Drewniak.cpp
@@ -2,40 +2,85 @@
 #include "Code_Search_Drewniak.h"
 using namespace std;
 
+
+
+
 search::search(){
-int i;
+	int i;
 
 	for (i = 0; i < 20; i++) {
 		array[i] = i;
 	}
 
-}
+	int j;
 
-void search::linear(int num){
-	int i;
-	int index = 0;
-	int calc_num = 0;
+	for (j = 0; j < 10000; j++) {
+		large_array[j] = j;
 
-
-	for (i = 0; i < 20; i++) {
-		if (array[i] == num);
-			calc_num = calc_num + 1;
-		if (index >= array[20] || index !=num);
-			
-
-	cout << "Number is: " << num << "\n";
-	cout << "Number of Calculations test: " << calc_num << "\n";
 
 	}
 
+
+
+
+}
+
+void search::linear(int num1){
+	int i;
+	int index = 0;
+
+
+	for (i = 0; i < 20; i++) {
+		if (array[i] == num1){
+				cout << "Number is: " << i << "\n";
+					break;
+
+		}
+	}
+
+
+}
+
+
+void search::binary(int num2){
+	int j;
+	int index = 0;
+
+
 }
 
 
 
-
+#include <ctime>
 int main() {
-int num = 6;
+/*
+clock_t begin = clock();
+
+code_to_time(); //code that will be timed
+
+clock_t end = clock();
+
+
+//CLOCKS_PER_SEC is the number of “clock ticks” per second
+cout << “Number of seconds: “ << double (begin - end) / CLOCKS_PER_SEC;
+*/
+
+
+
+
+
+
+
+
+int num1 = 6;
 	search linear;
-	linear.linear(num);
+	linear.linear(num1);
+
+/*
+int num2 = 4700;
+	search binary;
+	binary.binary(num2);
+*/
+
 	return 0;
 }
--- Code_Search_Drewniak.h
+++ Code_Search_Drewniak.h
@@ -6,11 +6,13 @@ class search {
 
 
 		//Accessors
-		void linear(int num); //searches for a number, num, using linear search
+		void linear(int num1); //searches for a number, num, using linear search
+		void binary(int num2); //searches for a number, num, using binary search
+
 
 
 
 	private:
 		int array[20]; //the array of numbers to be searched
-
+		int large_array[10000];
 };
